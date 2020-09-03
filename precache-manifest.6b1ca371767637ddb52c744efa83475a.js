6a815c50488bfeeeb1e170aa8d6895d8a6c4f21c
VilleKokkarinen <kokkarinen.ville@gmail.com>
2020-09-03 13:38:36 +0300

    Updates

---
+++ precache-manifest.6b1ca371767637ddb52c744efa83475a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a50217984c3ae5a86680",
+    "url": "/static/js/main.b3364a32.chunk.js"
+  },
+  {
+    "revision": "518df1a297d9976b7ad4",
+    "url": "/static/js/2.beeed752.chunk.js"
+  },
+  {
+    "revision": "a50217984c3ae5a86680",
+    "url": "/static/css/main.9bd43f9c.chunk.css"
+  },
+  {
+    "revision": "28242f09445cb206b5c7668116c08faf",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Narikka/precache-manifest.d3cf65d6b48673bcfbc6c61fd0afa913.js"
+  "/precache-manifest.6b1ca371767637ddb52c744efa83475a.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Narikka/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
